128139cf1d074ac35f53e619a0f2ee0828d9eb5b
sruud <13337210+sruud@users.noreply.github.com>
2025-02-08 20:06:19 -0700

    Filled out about me - skills sections - fa icons for now

---
+++ app/components/AboutMe.tsx
@@ -0,0 +1,34 @@
+export default function AboutMe() {
+  return (
+    <section id="about" className="max-w-4xl px-6 py-8">
+      <h2 className="text-3xl font-bold mb-6 text-left">About Me</h2>
+      <div className="about-me-container space-y-4">
+        <div className="about-me-text-container space-y-4">
+          <p>
+            Hi! I'm Steven. I am a web developer who is particularly good at
+            front-end projects and testing software. I built my skills up in a
+            time before AI agents were heavily incorporated into our workflows
+            and have spent countless hours debugging and troubleshooting issues.
+            I have professional experience maintaining legacy code, migrating
+            projects to new technologies, and automating repetitive tasks. Over
+            the years, I've contributed to Engineering, QA, and Customer Success
+            teams, giving me a well-rounded perspective on software development
+            and user needs.
+          </p>
+          <p>
+            In my free time, I like to read about the current state of web
+            development, experiment with TouchDesigner, and compose music. I
+            also enjoy indoor rock climbing because it allows you to map out a
+            plan and execute it on the fly.
+          </p>
+          <p>
+            I'm constantly learning and pushing myself to try new things. 2024
+            was the year when I participated in an in-person boot camp, joined
+            and collaborated with a team, and submitted my first project in a
+            hackathon ever at ETHDenver.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,22 @@
 import { BlogPosts } from "app/components/posts";
 import Intro from "./components/Intro";
 import AboutMe from "./components/AboutMe";
+import Skills from "./components/Skills";
 
 export default function Page() {
   return (
-    <section>
-      {/* <div className="intro flex items-center space-x-6">
-
-      </div> */}
-      {/* Remove */}
-      {/* <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1> */}
-      {/* Create new component and move this to the to an about me section */}
-      {/* <p className="mb-4">
-        {`Hi! I'm Steven. I am a web developer who is particularly good at
-            front-end projects and testing software. I built my skills up in a
-            time before AI agents were heavily incorporated into our workflows
-            and have spent countless hours debugging and troubleshooting issues.
-            I have professional experience maintaining legacy code, migrating
-            projects to new technologies, and automating repetitive tasks. Over
-            the years, I've contributed to Engineering, QA, and Customer Success
-            teams, giving me a well-rounded perspective on software development
-            and user needs.`}
-      </p> */}
+    <section className="relative min-h-screen flex flex-col text-white">
       <Intro />
       <AboutMe />
-      {/* Skills */}
-      <section className="max-w-4xl px-6 py-8">
-        <h2 className="text-3xl font-bold mb-6 text-left">Skills</h2>
-        <div
-          id="skills-container"
-          className="grid grid-cols-3 gap-6 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 text-3xl text-gray-300 justify-items-center"
-        >
-          <i className="fa-brands fa-react text-blue-400 text-2xl hover:text-gray-400"></i>
-          {/* <i className="fa-brands fa-ts text-blue-400 text-2xl hover:text-gray-400"></i> */}
-          <img
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
-            alt="TypeScript"
-            className="w-8 h-8 hover:opacity-80"
-          />
-        </div>
-      </section>
+      <Skills />
       {/* Projects */}
-      <section className="max-w-4xl px-6 py-8">
+      <section id="projects" className="max-w-4xl px-6 py-8">
         <h2 className="text-3xl font-bold mb-6 text-left">Projects</h2>
       </section>
       {/* Contact put at the bottom remove blog and footer */}
-      <section className="max-w-4xl px-6 py-8">
+      <section id="contact" className="max-w-4xl px-6 py-8">
         <h2 className="text-3xl font-bold mb-6 text-left">Contact</h2>
       </section>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
     </section>
   );
 }
