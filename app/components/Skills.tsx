28d4e3e81c5405ef5e459aaa122a40cf14dc2d28
sruud <13337210+sruud@users.noreply.github.com>
2025-02-09 13:29:49 -0700

    Refactored Skills and conditional rendering

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -12,11 +12,12 @@ export default function Skills() {
       iconClass: "fa-brands fa-css3-alt",
       color: "text-blue-500",
     },
-    // {
-    //   name: "TypeScript",
-    //   imgSrc:
-    //     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-    // },
+    // Check if want to use this or not
+    {
+      name: "TypeScript",
+      imgSrc:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    },
     {
       name: "JavaScript",
       iconClass: "fa-brands fa-js",
@@ -78,7 +79,7 @@ export default function Skills() {
     { name: "Ruby", iconClass: "fa-solid fa-gem", color: "text-red-500" },
     { name: "C++", iconClass: "fa-solid fa-code", color: "text-blue-500" },
     {
-      name: "Mobile Dev",
+      name: "Mobile Testing",
       iconClass: "fa-solid fa-mobile-screen",
       color: "text-gray-500",
     },
@@ -113,19 +114,31 @@ export default function Skills() {
         id="skills-container"
         className="grid grid-cols-3 gap-8 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 text-3xl text-gray-300 justify-items-center"
       >
-        {skills.map((skill, index) => (
-          <div key={index} className="relative group">
-            {/* Icon */}
-            <i
-              className={`${skill.iconClass} text-4xl ${skill.color} group-hover:text-white transition duration-300`}
-            />
-
-            {/* Tooltip */}
-            <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 scale-0 group-hover:scale-100 transition-transform bg-gray-800 text-white text-xs font-semibold px-2 py-1 rounded-md">
-              {skill.name}
-            </span>
-          </div>
-        ))}
+        {skills.map((skill, index) =>
+          skill.imgSrc ? (
+            <div key={index} className="relative group">
+              <img
+                key={index}
+                src={skill.imgSrc}
+                alt={skill.name}
+                className="w-8 h-8 hover:opacity-80"
+              />
+              <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 scale-0 group-hover:scale-100 transition-transform bg-gray-800 text-white text-xs font-semibold px-2 py-1 rounded-md">
+                {skill.name}
+              </span>
+            </div>
+          ) : (
+            <div key={index} className="relative group">
+              <i
+                key={index}
+                className={`${skill.iconClass} ${skill.color} text-3xl hover:text-gray-400`}
+              ></i>
+              <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 scale-0 group-hover:scale-100 transition-transform bg-gray-800 text-white text-xs font-semibold px-2 py-1 rounded-md">
+                {skill.name}
+              </span>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
