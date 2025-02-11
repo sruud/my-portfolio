a6a4694975703b3dd49c018e6ac0bc777fc2a5ff
sruud <13337210+sruud@users.noreply.github.com>
2025-02-10 22:34:53 -0700

    Should be setup to deploy to gh-pages now

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Export Next.js as a static site
-  basePath: "/sruud", // GitHub repo name
-  assetPrefix: "/sruud",
+  // distDir: "docs", // Save the exported site in the "docs" folder
+  basePath: "/my-portfolio",
+  assetPrefix: "/my-portfolio",
+  images: {
+    unoptimized: true, // Disable Next.js image optimization for GitHub Pages - just preventing build errors
+  },
+  trailingSlash: true, // - good for static sites
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
